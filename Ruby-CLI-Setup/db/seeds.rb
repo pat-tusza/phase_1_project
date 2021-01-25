594b07c3fcfe9b9c10d2b4fe9d88f4c32a9d1061
github username <github email address>
2021-01-25 17:32:54 -0500

    progress

--- Ruby-CLI-Setup/db/seeds.rb
+++ Ruby-CLI-Setup/db/seeds.rb
@@ -15,6 +15,63 @@ Mood.reset_pk_sequence
 Recipe.reset_pk_sequence
 
 ########### Ingredients ############
+vodka = Ingredient.create("Vodka")
+tequila = Ingredient.create("Tequila")
+whisky = Ingredient.create("Whisky")
+dk_rum = Ingredient.create("Dark Rum")
+lt_rum = Ingredient.create("Light Rum")
+gin = Ingredient.create("Gin")
+agave = Ingredient.create("Agave Syrup")
+simp_syrup = Ingredient.create("Simple Syrup")
+sour = Ingredient.create("Sour mix")
+t_sec= Ingredient.create("Triple Sec")
+l_juice= Ingredient.create("Lemon Juice")
+i_cube= Ingredient.create("Ice Cube")
+tonic= Ingredient.create("Tonic")
+cherry= Ingredient.create("Cherry")
+lime= Ingredient.create("Lime")
+cran_juice= Ingredient.create("Cranberry Juice")
+orange_juice= Ingredient.create("Orange Juice")
+lime_juice= Ingredient.create("Lime Juice")
+
+
+puts "🥃 🥃 🥃 🥃 "
+
+######## Moods ########
+happy = Mood.create("Happy")
+sad  = Mood.create("Sad")
+tense  = Mood.create("Tense")
+frisky  = Mood.create("Frisky")
+excited  = Mood.create("Excited")
+
+puts " 🙃🙃🙃🙃🙃  "
+
+########### Recipes #######
+gin_and_tonic = Recipe.create("Gin and Tonic", 1)
+long_island = Recipe.create("Long Island Iced Tea", 3)
+screw_driver = Recipe.create("Screw Driver", 1)
+whisky_sour = Recipe.create("Whisky sour", 5)
+margarita = Recipe.create("Margarita", 4)
+
+
+puts "🌶🌶🌶🌶🌶🌶 "
+
+####### User ######
+patrick = User.create("Patrick", "1234")
+mike = User.create("Mike", "we did it joe")
+chase = User.create("Chase", "flatiron321")
+eric = User.create("Eric", "follow deliverables")
+sylwia = User.create("Sylwia", "dum dum")
+
+puts "🎮🎮🎮 "
+
+####### Recipe Ingredients #########
+
+
+
+
+
+
 
 
 
