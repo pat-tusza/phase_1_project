6388797788115f558d4a962a05bf13dfd63e53aa
Patrick Tuszakowski <mrtall500@gmail.com>
2021-01-26 16:21:14 -0500

    more tests

--- Ruby-CLI-Setup/app/models/User.rb
+++ Ruby-CLI-Setup/app/models/User.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
 
     has_many :user_moods
     has_many :moods, through: :user_moods
+
+    def create_user_mood(mood)
+        UserMood.create(user_id: self.id, mood_id: mood)
+    end
+    
+    def $change_user_mood(int)
+        self.user_moods.first.update(mood_id: int)
+        self.reload
+    end
 end
--- Ruby-CLI-Setup/app/models/interface.rb
+++ Ruby-CLI-Setup/app/models/interface.rb
@@ -25,9 +25,21 @@ class Interface
     
     def exit_helper
         puts "Have a good day!"
+        sleep(1)
     end
 
     def log_in_helper
+        name = prompt.ask("Enter Username")
+        pass = prompt.ask("Enter Password")
+        if User.find_by(name: name, pass: pass)
+            puts "Welcome Back!!"
+            self.user = User.find_by(name: name, pass: pass)
+            mood_screen
+        else
+            puts "Incorrect Username or Password 🤭"
+            sleep(1)
+            log_in
+        end
     end
 
     def sign_up_helper
@@ -38,13 +50,55 @@ class Interface
         end
         pass = prompt.ask("Enter Password")
         self.user = User.create(name: name, pass: pass)
-        home_screen
+        # home_screen
     end
 
-    def home_screen
+    def mood_screen
+        binding.pry
+        prompt.select("How are you feeling today?") do |menu|
+            # binding.pry
+            menu.choice "Happy", -> {self.change_user_mood(1)}
+            menu.choice "Sad", -> {self.change_user_mood(2)}
+            menu.choice "Tense", -> {self.change_user_mood(3)}
+            menu.choice "Frisky", -> {self.change_user_mood(4)}
+            menu.choice "Excited", -> {self.change_user_mood(5)}
+        end
     end
+    
+    
+    # def connect_user(name, pass)
+    #     hug = User.find_by(name: name , pass: pass)
+    #     if hug = self.user
+
+
+    # # end
+    # def create_user_mood(mood)
+    #     hug = User.find_by(name: name , pass: pass)
+    #     if hug = self.user
+    #         UserMood.create(user_id: self.id, mood_id: mood)
+    #     end
+    # end
+    
+
+    # def create_user_mood(mood)
+    #     hug = User.find_by(name: name , pass: pass)
+    #         if hug = self.user
+    #         UserMood.create(user_id: self.id, mood_id: mood)
+    #         end
+    #     end
+    # end
+    
+    # def update_user_mood(int)
+    #     self.user_moods.first.update(mood_id: int)
+    #     self.reload
+    # end
+
+    # def home_screen
+    # end
+
+    # def main_menu
+    # end
 
 
 end
-
 
