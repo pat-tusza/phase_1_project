923f55ed30ee64be50d54f47ea510c1ffb45be64
Patrick Tuszakowski <mrtall500@gmail.com>
2021-01-26 12:19:06 -0500

    small fix

--- Ruby-CLI-Setup/app/models/interface.rb
+++ Ruby-CLI-Setup/app/models/interface.rb
@@ -1,6 +1,8 @@
 class Interface
-
+    
     attr_reader :prompt
+    attr_writer :user
+
 
     def intialize
         @prompt = TTY::prompt.new
@@ -8,31 +10,33 @@ class Interface
 
     def welcome
         puts "Welcome to Mixr!"
-    end
-
-    def log_in
-        prompt.select ("What would you like to do?") do |menu|
-            menu.choice "Log in" -> {log_in_helper}
-            menu.choice "Sign up" -> {sign_up_helper}
-            menu.choice "Exit" -> {exit_helper}
-        end
+        sleep(2)
+        log_in
     end
     
-
+    def log_in
+        prompt.select ("What would you like to do?") do |menu|
+            menu.choice "Log in", -> {log_in_helper}
+            menu.choice "Sign up", -> {sign_up_helper}
+            menu.choice "Exit", -> {exit_helper}
+        end
+    end   
+    
     def exit_helper
         puts "Have a good day!"
     end
 
     def sign_up_helper
-        name =prompt.ask ("Enter Username")
+        name =prompt.ask("Enter Username")
         while User.find_by(name: name)
             puts "This username is already taken."
-            name =prompt.ask ("Enter Username")
+            name = prompt.ask("Enter Username")
         end
-
-        pass =prompt.ask ("Enter Password")
-        self.user = User.create (name: name, pass: pass)
+        pass = prompt.ask ("Enter Password")
+        self.user = User.create(name: name, pass: pass)
     end
+
+
 end
 
 
