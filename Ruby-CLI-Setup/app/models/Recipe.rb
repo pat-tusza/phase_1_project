49189a40aef63ae205bda23ab59a34c51c2b2a53
Patrick Tuszakowski <mrtall500@gmail.com>
2021-01-26 11:23:26 -0500

    added TTY Prompt

--- Ruby-CLI-Setup/app/models/Recipe.rb
+++ Ruby-CLI-Setup/app/models/Recipe.rb
@@ -3,4 +3,10 @@ class Recipe < ActiveRecord::Base
     belongs_to :mood
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+
+    def self.random_recipe(id_num)
+        random = self.where(mood_id: id_num)
+        random.sample
+    end
+    
 end
